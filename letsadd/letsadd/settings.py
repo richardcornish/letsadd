f141cb937098eaefc8969006f090a26549f55e12
Richard Cornish <richardcornish@gmail.com>
2021-04-22 16:12:19 -0700

    Installed WhiteNoise

--- letsadd/letsadd/settings.py
+++ letsadd/letsadd/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'letsadd.urls'
@@ -133,6 +134,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # Heroku
