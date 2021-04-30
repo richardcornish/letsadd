fe74f78f2ef2f7d40a7a8c9a0bc247f9019d786f
Richard Cornish <richardcornish@gmail.com>
2021-04-29 22:46:42 -0700

    Added validation to random numbers

--- letsadd/rando/forms.py
+++ letsadd/rando/forms.py
@@ -11,3 +11,16 @@ class RandomForm(forms.Form):
         a = self.cleaned_data['min']
         b = self.cleaned_data['max']
         return randint(a, b)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        min_ = cleaned_data.get('min')
+        max_ = cleaned_data.get('max')
+
+        if min_ and min_:
+            if min_ ==  max_:
+                message = 'The minimum field is equal to the maximum field.'
+                raise forms.ValidationError(message, code='invalid')
+            elif min_ >  max_:
+                message = 'The minimum field is greater than the maximum field.'
+                raise forms.ValidationError(message, code='invalid')
