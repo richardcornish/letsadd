b36c731dce41d98d7df05b4d755eab623b65b6df
Richard Cornish <richardcornish@gmail.com>
2021-05-28 15:38:44 -0700

    Cleaned JavaScript style

--- letsadd/fetch/static/fetch/js/script.js
+++ letsadd/fetch/static/fetch/js/script.js
@@ -1,46 +1,47 @@
-var ajax = function (input_selector, url, results_selector, error_selector) {
+let ajax = function (input_selector, url, results_selector, error_selector) {
 
-    var input = document.querySelector(input_selector);
-    var results = document.querySelector(results_selector);
-    var error = document.querySelector(error_selector);
-    var no_results = 'No results.'
+    let input = document.querySelector(input_selector);
+    let results = document.querySelector(results_selector);
+    let error = document.querySelector(error_selector);
+    let no_results = "No results.";
 
-    var get_results = async (url) => {
-        var response = await fetch(url, {
+    let get_results = async (url) => {
+        let response = await fetch(url, {
             headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'X-Requested-With': 'XMLHttpRequest'
+                "Content-Type": "application/x-www-form-urlencoded",
+                "X-Requested-With": "XMLHttpRequest"
             }
         });
-        return await response.json();
+        return response.json();
     };
 
-    var render_results = (data, error) => {
-        if (data.hasOwnProperty('object_list')) {
-            error.innerHTML = '';
+    let render_results = (data, error) => {
+        if (data.hasOwnProperty("object_list")) {
+            error.innerHTML = "";
             if (data.object_list.length === 0) {
                 results.innerHTML = `<p>${no_results}</p>`;
             } else {
-                results.innerHTML = `<ul>${data.object_list.map(item => `<li><a href="${item.fields.get_absolute_url}">${item.fields.title}</a></li>`).join('')}</ul>`;
+                results.innerHTML = `<ul>${data.object_list.map(item => `<li><a href="${item.fields.get_absolute_url}">${item.fields.title}</a></li>`).join("")}</ul>`;
             }
-        } else if (data.hasOwnProperty('errors')) {
-            results.innerHTML = '';
-            for (var field in data.errors) {
-                var selector = document.querySelector('.error-id_' + field);
-                let lis = '';
-                for (var error of data.errors[field]) {
+        }
+        if (data.hasOwnProperty("errors")) {
+            results.innerHTML = "";
+            Object.keys(data.errors).forEach((field) => {
+                let selector = document.querySelector(".error-id_" + field);
+                let lis = "";
+                data.errors[field].forEach((error) => {
                     lis += `<li>${error}</li>`;
-                }
+                });
                 selector.innerHTML = `<ul>${lis}</ul>`;
-            }
-        };
+            });
+        }
     };
 
-    input.addEventListener('keyup', (event) => {
-        var params = new URLSearchParams();
+    input.addEventListener("keyup", (event) => {
+        let params = new window.URLSearchParams();
         params.append(input.name, input.value);
-        window.history.replaceState({}, '', `${location.pathname}?${params}`);
-        get_results(`${url}?${params}`).then(data => {
+        window.history.replaceState({}, "", `${location.pathname}?${params}`);
+        get_results(`${url}?${params}`).then((data) => {
             render_results(data, error);
         });
     });
