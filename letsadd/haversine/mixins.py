ee4117e6f0b197776f72516cf108b44f622f40c5
Richard Cornish <richardcornish@gmail.com>
2021-06-10 15:54:08 -0700

    Added additional context and flexibility to JSON mixin

--- letsadd/haversine/mixins.py
+++ letsadd/haversine/mixins.py
@@ -5,24 +5,34 @@ from django.template.defaultfilters import floatformat
 
 
 class JsonSingleObjectMixin:
+    json_suffix = '_json'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if 'object' in context:
-            context['object_json'] = json.dumps(json.loads(serializers.serialize('json', [context['object']], fields=('latitude', 'longitude')))[0]['fields'])
+            object_json = json.dumps(json.loads(serializers.serialize('json', [context['object']], fields=('latitude', 'longitude')))[0]['fields'])
+            context['object%s' % self.json_suffix] = object_json
+            context_object_name = self.get_context_object_name(context['object'])
+            if context_object_name:
+                context['%s%s' % (context_object_name, self.json_suffix)] = object_json
         return context
 
 
 class JsonMultipleObjectMixin:
+    json_suffix = '_json'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if 'object_list' in context:
             object_list = json.loads(serializers.serialize('json', context['object_list'], fields=('name', 'latitude', 'longitude')))
             for obj in object_list:
-                qs_object = context['object_list'].get(pk=obj['pk'])
-                if hasattr(qs_object, 'distance'):  # check for annotated QuerySet
-                    obj['fields']['distance'] = floatformat(qs_object.distance, arg='0g')
-                obj['fields']['get_absolute_url'] = qs_object.get_absolute_url()
-            context['object_list_json'] = json.dumps([obj['fields'] for obj in object_list])
+                obj_qs = context['object_list'].get(pk=obj['pk'])
+                if hasattr(obj_qs, 'distance'):  # check for annotated QuerySet
+                    obj['fields']['distance'] = floatformat(obj_qs.distance, arg='0g')
+                obj['fields']['get_absolute_url'] = obj_qs.get_absolute_url()
+            object_list_json = json.dumps([obj['fields'] for obj in object_list])
+            context['object_list%s' % self.json_suffix] = object_list_json
+            context_object_name = self.get_context_object_name(context['object_list'])
+            if context_object_name:
+                context['%s%s' % (context_object_name, self.json_suffix)] = object_list_json
         return context
