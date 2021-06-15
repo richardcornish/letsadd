173b95944421de8d766d9975b9048b8977b22726
Richard Cornish <richardcornish@gmail.com>
2021-06-15 13:37:02 -0700

    Added function-based view equivalents

--- letsadd/agents/urls.py
+++ letsadd/agents/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import AgentDetailView, AgentListView
+from .views import AgentDetailView, AgentListView, agent_detail, agent_list
 
 
 app_name = 'agents'
 
 urlpatterns = [
-    path('<int:pk>', AgentDetailView.as_view(), name='agent_detail'),
-    path('', AgentListView.as_view(), name='agent_list'),
+    # path('<int:pk>', AgentDetailView.as_view(), name='agent_detail'),
+    # path('', AgentListView.as_view(), name='agent_list'),
+    path('<int:pk>', agent_detail, name='agent_detail'),
+    path('', agent_list, name='agent_list'),
 ]
--- letsadd/agents/views.py
+++ letsadd/agents/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Sum
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import DetailView, ListView
 
 from .models import Agent
@@ -22,3 +23,19 @@ class AgentListView(ListView):
         context = super().get_context_data(**kwargs)
         context['aggregate'] = self.get_queryset().aggregate(Sum('invoice__invoice_amount'))
         return context
+
+
+def agent_detail(request, pk):
+    obj = get_object_or_404(Agent, pk=pk)
+    return render(request, 'agents/agent_detail.html', {
+        'object': obj,
+        'aggregate': obj.invoice_set.aggregate(Sum('invoice_amount')),
+    })
+
+
+def agent_list(request):
+    qs = Agent.objects.prefetch_related('invoice_set').annotate(Sum('invoice__invoice_amount'))
+    return render(request, 'agents/agent_list.html', {
+        'object_list': qs,
+        'aggregate': qs.aggregate(Sum('invoice__invoice_amount')),
+    })
